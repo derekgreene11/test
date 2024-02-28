6deefc202333d33490155ce4dbebf6c015faa3c8
derekgreene11 <derekgreene11@gmail.com>
2024-02-28 13:42:19 -0800

    Update

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import os
+from colorama import Fore, Style
 
 class Product:
     def __init__(self, name, price, id, quantity):
@@ -8,19 +9,22 @@ class Product:
         self.name = name
     
     def getProductInfo(self):
+        loadFromFile()
         if len(list) >= 1:
             for self in list:
-                print("Name: ", self.name, "\nPrice: ", self.price, "\nQuantity: ", self.quantity.rstrip('\n'), "\nProduct ID: ", self.id, "\n")
-            else:
-                print("No products to display!")
+                print(f"{Fore.LIGHTCYAN_EX}{Style.NORMAL}Name: ", f"{Fore.CYAN}", self.name, f"{Style.RESET_ALL}{Fore.LIGHTGREEN_EX}{Style.NORMAL}\nPrice: ", f"{Fore.GREEN}", self.price, f"{Style.RESET_ALL}{Fore.LIGHTMAGENTA_EX}\nQuantity: ", f"{Fore.MAGENTA}", self.quantity.rstrip('\n'), f"{Style.RESET_ALL}{Fore.LIGHTYELLOW_EX}\nProduct ID: ", f"{Fore.YELLOW}", self.id, f"{Style.RESET_ALL}", "\n")
+        #else:
+            #print(f"{Fore.RED}No products to display!{Style.RESET_ALL}")
          
     def addProduct(self, list):
-        product = Product(name = input("Enter product name: "), price = input("Enter price: "), id = int(index1 + 1), quantity = input("Enter the quantity: "))
+        product = Product(name = input(f"{Fore.BLUE}Enter product name: "), price = "$" + input(f"Enter price: {Fore.GREEN}$"), id = int(index1 + 1), quantity = input(f"{Fore.BLUE}Enter the quantity: {Fore.MAGENTA}"))
         list.append(product)
         
     def removeProduct(self):
-        index = int(input("Enter the product ID of the product to remove: "))
-        if 0 < index:
+        index = int(input(f"{Fore.GREEN}Enter the product ID of the product to remove: "))
+        print('\n')
+        while 0 < index <= len(list):
+            print(f"{Fore.RED}", list[index-1].name, f"{Fore.GREEN}successfully removed!")
             with open(filePath,'rb') as read_file:
                 lines = read_file.readlines()
                 currentLine = 1
@@ -31,22 +35,25 @@ class Product:
                     else:
                         write_file.write(line)
                     currentLine += 1
+            break
         else:
-            print("Product ID entered does not exist!")          
+            print(f"{Fore.RED}Product ID entered does not exist!{Style.RESET_ALL}")        
             
     def changeQuantity(self):
-        newQuantity = (input("Enter the product ID of the product to update: "))
-        if 0 <= newQuantity < len(list):
-            productUpdate = input("Enter new quantity: ")
-            list[newQuantity].quantity = productUpdate
+        newQuantity = int(input(f"{Fore.MAGENTA}Enter the product ID of the product to update: {Style.RESET_ALL}"))
+        if 0 < newQuantity <= len(list):
+            print(f"{Fore.GREEN}Current quantity:", list[newQuantity-1].quantity)
+            productUpdate = input(f"{Fore.LIGHTYELLOW_EX}Enter new quantity: ")
+            list[newQuantity-1].quantity = productUpdate
+            print("\n")
+            print(f"{Fore.MAGENTA}", list[newQuantity-1].name, f"quantity successfully updated!{Fore.RED}")
         else:
-            print("Product ID entered does not exist!")
+            print(f"Product ID entered does not exist!{Style.RESET_ALL}")
 
     def saveTofile(self):
         os.remove(filePath)
         for self in list:
             stringArr = [self.name, " ", self.price, " ", str(self.id), " ", self.quantity.rstrip('\n'), "\n"]
-            print(stringArr)
             with open(filePath, "ab") as file:
                 for string in stringArr:
                     byteArray = string.encode()
@@ -55,27 +62,33 @@ class Product:
     
 def loadFromFile():
     i = 0
+    list.clear()
     with open(filePath, "rb") as file:
         for string in file:
             byteArray = string.decode()
             data = byteArray.split(" ", 4)
-            list.append(Product(data[0], data[1], data[2], data[3]))
-            i = i + 1
-    file.close() 
-    global index1 
-    index1 = int(list[i-1].id)
-    return index1
+            list.append(Product(data[0], data[1], i+1, data[3]))
+            i += 1
+    file.close()
+    if i > 0: 
+        global index1 
+        index1 = int(list[i-1].id)
+        return index1
+    else:
+        index1 = 1
+        os.system('cls')
+        print(f"{Fore.GREEN}{Style.BRIGHT}Product Catalog 1.0{Style.RESET_ALL}\n")
+        print(f"{Fore.RED}No products in product.bin file!{Fore.MAGENTA}\n")
                 
 def menu():
     os.system('cls')
-    print("Product Catalog 1.0")
-    print("-------Menu------")
-    print("\n1: List Products")
+    print(f"{Fore.GREEN}{Style.BRIGHT}Product Catalog 1.0{Style.RESET_ALL}")
+    print(f"{Fore.WHITE}{Style.DIM}-------Menu------{Style.RESET_ALL}")
+    print(f"{Fore.MAGENTA}{Style.BRIGHT}\n1: List Products")
     print("2: Add Product")
     print("3: Remove Product")
     print("4: Change Quantity")
-    print("5: Save File")
-    print("6: Exit\n")
+    print(f"5: Exit\n{Style.RESET_ALL}")
     
     choice = input("")
     while choice != 5:
@@ -83,41 +96,51 @@ def menu():
             case "1":
                 os.system('cls')
                 product.getProductInfo()
-                input("Press any key to continue... ")
+                input(f"{Fore.MAGENTA}Press any key to continue... {Style.RESET_ALL}")
                 os.system('cls')
                 menu()   
             case "2":
                 os.system('cls')
                 product.addProduct(list)
-                input("Press any key to continue... ")
+                product.saveTofile()
+                input(f"{Fore.MAGENTA}Press any key to continue... {Style.RESET_ALL}")
                 os.system('cls')
                 menu()
             case "3":
                 os.system('cls')
-                product.removeProduct()
-                input("Press any key to continue... ")
-                os.system('cls')
-                menu()
+                loadFromFile()
+                if index1 > 0:
+                    product.removeProduct()
+                    input(f"{Fore.MAGENTA}\nPress any key to continue... {Style.RESET_ALL}")
+                    loadFromFile()
+                    product.saveTofile()
+                    os.system('cls')
+                    menu()
+                else: 
+                    os.system("cls")
+                    print(f"{Fore.GREEN}{Style.BRIGHT}Product Catalog 1.0{Style.RESET_ALL}\n")
+                    print(f"{Fore.RED}No products in product.bin file!{Fore.MAGENTA}\n")
+                    input(f"{Fore.MAGENTA}\nPress any key to continue... {Style.RESET_ALL}")
+                    menu()
             case "4":
                 os.system('cls')
-                product.changeQuantity()
-                input("Press any key to continue... ")
-                os.system("cls")
-                menu()
-            case "5":
+                if index1 > 0:
+                    product.changeQuantity()
+                    product.saveTofile()
+                    input(f"{Fore.MAGENTA}Press any key to continue... {Style.RESET_ALL}")
+                    os.system("cls")
+                    menu()
+            case "5": 
                 os.system('cls')
                 product.saveTofile()
-                print("File saved!")
-                input("Press any key to continue... ")
-                os.system('cls')
-                menu()
-            case "6": 
-                os.system('cls')
-                product.saveTofile()
-                print("Goodbye!")
+                print(f"{Fore.LIGHTCYAN_EX}Goodbye!{Style.RESET_ALL}")
                 break
+            case _:
+                os.system('cls')             
+                print(f"{Fore.RED}Invalid menu choice!\n{Fore.MAGENTA}")
+                input(f"Press any key to continue... {Style.RESET_ALL}")
+                menu()
         return 0
-
 filePath = "product.bin"       
 list = []
 product = Product("name", "price", 0, "quantity")
